8384c163c318ec5fe97be6d7fed3d5753c4ddeae
march <lmarch@yandex.ru>
2024-11-29 01:07:19 +0300

    homework8

---
+++ src/main/java/com/march/project/homework8/WorkingWithArray.java
@@ -0,0 +1,40 @@
+package com.march.project.homework8;
+
+import com.march.project.homework8.errors.AppArrayDataException;
+import com.march.project.homework8.errors.AppArraySizeException;
+
+public class WorkingWithArray {
+    public static void main(String[] args) {
+        String[][] matrix = { {"1", "1", "1", "е"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"} };
+
+        try {
+            int result = checkingArrayForDimension(matrix);
+            System.out.println(result);
+        } catch (AppArraySizeException ex) {
+            System.out.println(ex.getMessage());
+        } catch (AppArrayDataException ex) {
+            System.out.println(ex.getMessage() + ex.getLine() + ex.getColumn());
+        }
+    }
+
+    public static int checkingArrayForDimension(String[][] arr) throws AppArraySizeException, AppArrayDataException {
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr.length != 4) {
+                throw new AppArraySizeException("The array must have a size of 4x4");
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i].length != 4) {
+                    throw new AppArraySizeException("The array must have a size of 4x4");
+                }
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException ex) {
+                    throw new AppArrayDataException("Невозможно преобразовать строку в число. Строка и столбец: ", i, j);
+                }
+            }
+        }
+        return sum;
+    }
+}
